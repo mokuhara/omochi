7f515871e8486449c8262ae38722a5840573d43c
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2020-11-19 01:19:23 +0900

    serviceAuth作成

---
+++ app/service/auth.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"log"
+	"omochi/app/models"
+	"strings"
+	"time"
+)
+
+type TokenService struct {}
+
+type Auth struct {
+	UserId int64
+	UserType int64
+	Iat int64
+}
+
+const (
+	secret = "VgHIOzK076FnCHA3NYgrJ2fZdfr9y5RRV5XBgwqvgNzNNop/7jC7Bg=="
+	userIDKey = "user_id"
+	userType = "type"
+	iatKey =    "iat"
+	expKey =    "exp"
+	lifetime =  30 * time.Minute
+)
+
+func (TokenService) Generate(user *models.User, now time.Time) (string, error){
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		userIDKey: user.ID,
+		userType: user.Type,
+		iatKey: now.Unix(),
+		expKey: now.Add(lifetime).Unix(),
+	})
+	return token.SignedString([]byte(secret))
+}
+
+func (TokenService) Verify(c *gin.Context) (*Auth, error){
+	authHeader := c.Request.Header["Authorization"][0]
+	bearerToken := strings.Split(authHeader, " ")
+	if len(bearerToken) == 2{
+		authToken := bearerToken[1]
+		token, err := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error){
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("token verify error")
+			}
+			return []byte(secret), nil
+		})
+		if err != nil {
+			log.Printf("action=Verify err=%s", err.Error())
+			return nil, err
+		}
+		if token.Valid {
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok {
+				log.Printf("action=Verify not found claims in %s", authToken)
+				return nil, fmt.Errorf("not found claims in %s", authToken)
+			}
+			userId, ok := claims[userIDKey].(float64)
+			if !ok {
+				log.Printf("action=Verify not found %s in %s", userIDKey, authToken)
+				return nil, fmt.Errorf("not found %s in %s", userIDKey, authToken)
+			}
+			userType, ok := claims[userType].(float64)
+			if !ok {
+				log.Printf("action=Verify not found %s in %s", userType, authToken)
+				return nil, fmt.Errorf("not found %s in %s", userType, authToken)
+			}
+			iat, ok := claims[iatKey].(float64)
+			if !ok {
+				log.Printf("action=Verify not found %s in %s", iatKey, authToken)
+				return nil, fmt.Errorf("not found %s in %s", iatKey, authToken)
+			}
+			return &Auth{
+				UserId: int64(userId),
+				UserType: int64(userType),
+				Iat: int64(iat),
+			}, nil
+		} else {
+			log.Println("failed token valid")
+			return nil, fmt.Errorf("failed token valid")
+		}
+	} else {
+		log.Println("invalid token")
+		return nil, fmt.Errorf("invalid token")
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module omochi
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/jinzhu/gorm v1.9.16
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
