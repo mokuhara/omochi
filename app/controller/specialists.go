3f5b5c1eba9d46312e1c60e14c3066cf34bb6527
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2020-11-19 15:39:49 +0900

    controller_specialist作成

--- app/controller/specialists.go
+++ app/controller/specialists.go
@@ -1,3 +1,22 @@
 package controller
 
-//TODO
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"omochi/app/repository"
+)
+
+func GetAllProfile(c *gin.Context){
+	profileRepository := repository.ProfileRepository{}
+	profiles, err := profileRepository.GetAll()
+	if err != nil {
+		log.Println("action=GetAllProfile failed to get profiles")
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(http.StatusInternalServerError)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"data": profiles,
+	})
+}
