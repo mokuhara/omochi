d71498f544c3f222c075b4fa6f2b6d7637e86040
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2020-11-19 09:34:15 +0900

    controller_auth作成

--- app/controller/auth/auth.go
+++ app/controller/auth/auth.go
@@ -1,1 +1,100 @@
 package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+	"omochi/app/models"
+	"omochi/app/repository"
+	"omochi/app/service"
+	"time"
+)
+
+func Signup(c *gin.Context){
+	user := models.User{}
+	err := c.BindJSON(&user)
+	if err != nil {
+		log.Println("bind error")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+	purePassword := user.Password
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		log.Println("failed to create password hash")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+	user.Password = string(hash)
+	userRepository := repository.UserRepository{}
+	err = userRepository.Create(&user)
+	if err != nil {
+		log.Println("failed to create user")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+	//passwordをhash化前に戻す
+	user.Password = purePassword
+	token, err :=  createToken(&user, c)
+	if err != nil {
+		log.Println("failed to create token")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+
+	data := map[string]string{"token": token}
+	c.JSON(http.StatusCreated, gin.H{
+		"status": http.StatusOK,
+		"data": data,
+	})
+}
+
+func Login(c *gin.Context){
+	user := models.User{}
+	err := c.BindJSON(&user)
+	if err != nil {
+		log.Println("bind error")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+	token, err := createToken(&user, c)
+	if err != nil {
+		log.Println("failed to create token")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return
+	}
+
+	data := map[string]string{"token": token}
+	c.JSON(http.StatusCreated, gin.H{
+		"status": http.StatusOK,
+		"data": data,
+	})
+}
+
+
+func createToken(user *models.User, c *gin.Context) (string, error){
+	userRepository := repository.UserRepository{}
+	matchUser, err := userRepository.GetByEmail(user.Email)
+	if err != nil {
+		log.Println("failed to get user by email")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(matchUser.Password), []byte(user.Password))
+	if err != nil {
+		log.Println("invalid password")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return "", err
+	}
+
+	tokenService := service.TokenService{}
+	token, err := tokenService.Generate(matchUser, time.Now())
+	if err != nil {
+		log.Println("failed create token")
+		c.Error(err).SetType(gin.ErrorTypePublic)
+		return "", err
+	}
+	return token, nil
+}
--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/jinzhu/gorm v1.9.16
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
 	gopkg.in/ini.v1 v1.62.0
 )
